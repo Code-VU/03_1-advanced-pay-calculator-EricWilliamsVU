e10ae39458b01ae4da63d66b6be53ca9e901e7bb
Eric Williams <eric.williams@vu.com>
2023-02-06 17:26:26 -0600

    IDK what I'm doing wrong

--- payCalculator.py
+++ payCalculator.py
@@ -1,7 +1,31 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = input("Enter Hours: ")
     
+    try:
+        hrs = float(hrs)
+    except:
+        print("Error, please enter numeric input")
+    
+    rate = input("Enter Rate: ")
+
+    try:
+        rate = float(rate)
+        
+        if hrs > 40:
+            overtime = float(hrs - 40)
+            pay = float((40 * rate) + (rate * 1.5 * overtime))
+        else:
+            pay = float(hrs * rate)
+        
+        print(f"Pay: {pay}")
+    
+    except:
+        print("Error, please enter numeric input")
+    
+    
+
+
     # end assignment
 
 ## if you want to test locally before you try to sync
